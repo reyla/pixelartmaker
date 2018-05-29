512d5d2b8ad8349bc5c3336a8532c9ce78622ceb
Kate Speed <katespeed@gmail.com>
2018-05-28 21:02:46 -0400

    Add cleaner commenting to js file

--- designs.js
+++ designs.js
@@ -1,34 +1,39 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
-
+/**
+* @description builds table of cells based on user input for height and width
+*/
 function makeGrid() {
   var height = $('#inputHeight').val();
   var width = $('#inputWidth').val();
   var table = document.getElementById('pixelCanvas');
   // clear any existing grid cells
   $('#pixelCanvas').children().remove();
-  // function below adds table rows and cells to create the grid
-  for(let x = 0; x < height; ++x) {
+  // this loop adds table rows and cells to create the grid
+  for (let x = 0; x < height; ++x) {
     var row = table.insertRow(0);
-    for(let i = 0; i < width; ++i) {
+    for (let i = 0; i < width; ++i) {
     var cell = row.insertCell(0);
     }
   }
 }
 
-//make sure document is fully loaded
-$(function(){
-  // submit button runs the grid build
+
+/**
+* @description these functions only work once DOM is fully loaded
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  /**
+  * @description submit button runs the grid function
+  */
   $('#sizePicker').submit(function(event) {
     event.preventDefault();
     makeGrid();
-  });
-  // grid cell changes to selected color when user clicks on it
+  })
+  /**
+  * @description grid cell changes to selected color when user clicks on it
+  */
   $('#pixelCanvas').on('click', 'td', function (){
     const color = $('#colorPicker').val();
     $(this).css('background-color', color);
-  });
-})
+  })
+});
